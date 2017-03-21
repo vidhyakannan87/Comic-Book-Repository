2815a4535c0b07bad2932b435ac6ffe65cac7bf4
vidhyakannan87 <vidhyakannan87@gmail.com>
2017-03-21 13:11:08 +1100

    Added a Action method

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController:Controller
     {
+        public ActionResult Detail()
+        {
+            return Content("Hello from Comic World!!");
+        }
     }
 }
